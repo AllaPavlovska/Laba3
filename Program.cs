a79529303dec3483a642be716c4d54559af15990
AllaPavlovska <pavlovskaa87@gmail.com>
2025-03-11 17:29:22 -0400

    клас ScheduleEntry

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class ScheduleEntry
+{
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+    public string Location { get; set; }
+    public string Comment { get; set; }
+
+    public ScheduleEntry(DateTime start, DateTime end, string location, string comment)
+    {
+        StartTime = start;
+        EndTime = end;
+        Location = location;
+        Comment = comment;
+    }
+
+    public override string ToString()
+    {
+        return $"{StartTime} - {EndTime} | {Location} | {Comment}";
+    }
+}
